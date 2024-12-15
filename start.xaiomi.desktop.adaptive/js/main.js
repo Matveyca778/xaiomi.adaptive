170dfc1520d3211cb3992837390278a43b853378
Matveyca778 <sergeys@senthy.com>
2024-12-15 17:00:52 +0700

    Add files via upload

---
+++ start.xaiomi.desktop.adaptive/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  
+  $('.bike-slider, .slider__items').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoPlay: true,
+    autoPlaySpeed: 2000,
+    
+    
+  });
+
+
+});
